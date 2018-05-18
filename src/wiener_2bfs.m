62d4133187af9ec912b1e7b71740cf45a94478b3
Roland Hostettler <roland.hostettler@aalto.fi>
2018-05-18 15:18:36 +0300

    Finished re-implementation of Wiener 2 bootstrap filter smoother.

--- src/wiener_2bfs.m
+++ src/wiener_2bfs.m
@@ -6,16 +6,33 @@ function [xhat, sys] = wiener_2bfs(y, t, model, M, par)
 %   [xhat, sys] = WIENER_2BFS(y, t, model, M, par, sys)
 %
 % DESCRIPTION
-%   
+%   Bootstrap particle filter based two filter smoother for Wiener
+%   state-space systems as discussed in [1].
 %
 % PARAMETERS
-% 
+%   y       Ny*N matrix of measurement data.
+%   t       1*N vector of timestamps.
+%   model   Wiener state-space model structure.
+%   M       Number of particles (default: 100).
+%   par     Additional parameters:
+%
+%               Mt  Resampling threshold (default: M/3).
 %
 % RETURNS
+%   xhat    Smoothed state estimate.
+%   sys     Particle system structure (array of structs).
 %
+% REFERENCES
+%   [1] R. Hostettler, “A two filter particle smoother for Wiener state-
+%       space systems,” in IEEE Conference on Control Applications (CCA),
+%       Sydney, Australia, September 2015.
 % 
 % AUTHORS
-%   2018-05-18 -- Roland Hostettler <roland.hostettler@aalto.fi>   
+%   2018-05-18 -- Roland Hostettler <roland.hostettler@aalto.fi>
+
+% TODO
+%   * Smoothing of initial state is missing (needs special attention in the
+%     smoothed weight section).
    
     %% Defaults & Checks
     narginchk(3, 5);
@@ -25,7 +42,9 @@ function [xhat, sys] = wiener_2bfs(y, t, model, M, par)
     if nargin < 5
         par = [];
     end
-    def = struct();
+    def = struct( ...
+        'Mt', M/3 ...   % Resampling threshold
+    );
     par = parchk(par, def);
     
     %% Process data
@@ -90,130 +109,74 @@ function sys = filter(y, t, model, M, par)
 end
 
 %% Backward filter and smoothing
-function [xhat, Phat, sys] = smooth(y, t, model, M, par, sys, mu_x, Sigma_x)
-    py = model.py;
-
-    %% Preallocate
-    [Nx, M, N] = size(sys.x);
-    xhat = zeros(Nx, N);
-    Phat = zeros(Nx, Nx, N);
-    if nargout == 3
-        sys.xs = zeros(Nx, M, N);
-        sys.wb = zeros(1, M, N);
-        sys.lwb = zeros(1, M, N);
-        sys.ws = zeros(1, M, N);
-        sys.lws = zeros(1, M, N);
-    end
+function [xhat, sys] = smooth(y, t, model, M, par, sys)
+    %% Initialization
+    Nx = size(model.m0, 1);
+    N = size(y, 2);
+    x = sys(N).x;
+    lwb = ( ...
+        log(sys(N).w) + logmvnpdf(x.', sys(N).mu.', sys(N).Sigma.').' ...
+        - log(sys(N-1).w) - logmvnpdf(x.', (model.F(t(N))*sys(N-1).x).', model.Q(t(N)).').' ...
+    );
+    wb = exp(lwb-max(lwb));
+    wb = wb/sum(wb);
+    lwb = log(wb);
     
-    % Recurring temporary variables
-    % TODO: sort out
-    Ny = size(y, 1);
-    mu_xb = zeros(Nx, M);
-    mu_yb = zeros(Ny, M);
-    B = zeros(Nx, Ny, M);
-    S = zeros(Ny, Ny, M);
-    
-    lv = zeros(1, M);
-
-    %% Initialize Backward Filter
+    % Calculate the smoothed weights
+    s = zeros(1, M);
     for m = 1:M
-        lv(:, m) = logmvnpdf(sys.x(:, m, N).', mu_x(:, N).', Sigma_x(:, :, N)).' ...
-            + py.logpdf(y(:, N), sys.x(:, m, N), t(N)) - sys.lw(:, m, N);
+        s = s + sys(N-1).w(:, m)*mvnpdf(x.', (model.F(t(N))*sys(N-1).x(:, m)).', model.Q(t(N)).').';
     end
-    v = exp(lv-max(lv));
-    v = v/sum(v); 
-    beta = sysresample(v);
-    xs = sys.x(:, beta, N);
-    lwb = log(1/M)*ones(1, M);
-    wb = 1/M*ones(1, M);
-    lws = log(1/M)*ones(1, M);
-    ws = 1/M*ones(1, M);
-    xhat(:, N) = mean(xs, 2);
+    lws = lwb + log(s) - logmvnpdf(x.', sys(N).mu.', sys(N).Sigma.').';
+    ws = exp(lws-max(lws));
+    ws = ws/sum(ws);
     
-    % Store the particle system
-    if nargout == 3
-        sys.xs(:, :, N) = xs;
-        sys.lwb(:, :, N) = lwb;
-        sys.wb(:, :, N) = wb;
-        sys.lws(:, :, N) = lws;
-        sys.ws(:, :, N) = ws;
-    end
+    % Store
+    sys(N).xs = x;
+    sys(N).ws = ws;
+    sys(N).wb = wb;
+    sys(N).r = false;
 
-    %% Backward-Filter & Smoothing
-    for n = N-1:-1:1
-        %% Backward Filter
-        % Calcualte backward dynamics
-        F = model.F(t);
-        Q = model.Q(t);
-        L = F*Sigma_x(:, :, n)*F' + Q;
-        K = Sigma_x(:, :, n)*F'/L;
-        mu_xb = mu_x(:, n)*ones(1, M) + K*(xs - (F*mu_x(:, n))*ones(1, M));
-        Sigma_xb = Sigma_x(:, :, n) - K*L*K';
+    % Point estimate
+    xhat = zeros(Nx, N);
+    xhat(:, N) = x*ws';
+
+    %% Backward recursion
+    for n = N-1:-1:2
+        % Resample
+        [alpha, lwb, r] = resample_ess(lwb, par);
         
-        % Decompose 
-        Nq = rank(Sigma_xb);
-        [V, D] = eig(Sigma_xb);
-        D = diag(D);
-        D(abs(D)/max(abs(D)) < 1e-6) = 0; % To avoid numerical problems, discard eigenvalues that are smaller epsilon*max(abs(D))
-        [~, indices] = sort(D, 'descend');
-        Phi = diag(D(indices(1:Nq)));
-        Gamma = V(:, indices(1:Nq));
+        % Draw new particles
+        F = model.F(t(n+1));
+        K = sys(n).Sigma*F'/sys(n+1).Sigma;
+        mu = sys(n).mu*ones(1, M) + K*(x(:, alpha) - sys(n+1).mu*ones(1, M));
+        Sigma = sys(n).Sigma - K*sys(n).Sigma*K';
+        xp = mu + chol(Sigma).'*randn(Nx, M);
         
-        % Draw new samples
-        xs = mu_xb + Gamma*sqrt(Phi)*randn(Nq, M);
-        
-        % Weigh
-        if py.fast
-            lwb = lwb + py.logpdf(y(:, n)*ones(1, M), xs, t(n));
-        else
-            for m = 1:M
-                lwb(m) = lwb(m) + py.logpdf(y(:, n), xs(:, m), t(n));
-            end
-        end
+        % Calculate backward filter weights
+        lv = calculate_incremental_weights_bootstrap(y(:, n), xp, [], t(n), model, []);
+        lwb = lwb+lv;
         wb = exp(lwb-max(lwb));
         wb = wb/sum(wb);
         lwb = log(wb);
-        
-        if max(abs(imag(wb))) ~= 0
-            aaa = 1;
-        end
+        x = xp;
 
-        %% Smoothing
-if 0
-        if n > 1
-            x = sys.x(:, :, n-1);
-            w = sys.w(:, :, n-1);
-        else
-            x = sys.x0;
-            w = 1/M*ones(1, M);
-        end
-        nu = zeros(1, M);
+        % Calculate smoothed weights
+        s = zeros(1, M);
         for m = 1:M
-            nu(m) = w*mvnpdf(xs(:, m).', (F*x).', Q);
-%             tmp = lw + logmvnpdf(xs(:, m).', (F*x).', Q).';
-%             lnu(m) = log(sum(exp(tmp)));
+            s = s + sys(n-1).w(:, m)*mvnpdf(x.', (model.F(t(n))*sys(n-1).x(:, m)).', model.Q(t(n)).').';
         end
-        lws = lwb - logmvnpdf(xs.', mu_x(:, n).', Sigma_x(:, :, n)).' + log(nu);
+        lws = lwb + log(s) - logmvnpdf(x.', sys(n).mu.', sys(n).Sigma.').';
         ws = exp(lws-max(lws));
         ws = ws/sum(ws);
-        lws = log(ws);
-        xhat(:, n) = xs*ws.';
-end
-
-        %% Store
-        if nargout == 3
-            sys.xs(:, :, n) = xs;
-            sys.lwb(:, :, n) = lwb;
-            sys.wb(:, :, n) = wb;
-            sys.lws(:, :, n) = lws;
-            sys.ws(:, :, n) = ws;
-        end
+                
+        % Point estimate
+        xhat(:, n) = x*ws';
         
-        %% Resample Backward Filter
-        % TODO: We only need to resample when ESS < Threshold (or similar)
-        ir = sysresample(wb);
-        xs = xs(:, ir);
-        wb = 1/M*ones(1, M);
-        lwb = log(1/M)*ones(1, M);
+        % Store the particles and their weights
+        sys(n).xs = x;
+        sys(n).wb = wb;
+        sys(n).ws = ws;
+        sys(n).rs = r;
     end
 end
