78ecf30deab23f49efaa4fd9e57fe16c51b3676b
Roland Hostettler <r.hostettler@gmx.ch>
2019-07-09 09:21:01 +0200

    Removed old files from index.

--- incoming/utils/modelchk.m
+++
@@ -1,9 +0,0 @@
-function [px, py, px0] = modelchk(model)
-% 
-
-    % Implement such that we automagically generate missing function (e.g.
-    % logpdf)
-    px = model.px;
-    py = model.py;
-    px0 = model.px0;
-end
