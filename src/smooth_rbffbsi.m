d6fe500c60fd399c4de421bc39f6c9fa29f1b4e2
Roland Hostettler <r.hostettler@gmx.ch>
2021-09-21 14:17:14 +0200

    Major rewrite of RB-FFBSi function.

--- src/smooth_rbffbsi.m
+++ src/smooth_rbffbsi.m
@@ -13,21 +13,18 @@ function [shat, zhat, sys] = smooth_rbffbsi(model, y, theta, Js, sys, par)
         par = struct();
     end
     def = struct(...
+        'predict_bf', @predict_bf_hierarchical, ...
     	'sample_backward_simulation', @sample_backward_simulation ...
     );
     par = parchk(par, def);
     
     %% Preallocate
+    % Get dimensions
     [dy, N] = size(y);
     [ds, Jf] = size(sys(N).x);
-    dz = size(sys(N).mz, 1);
     
+    % Preallocate
     shat = zeros(ds, N);
-    lambda_hat = zeros(dz, Js);
-    Omega_hat = zeros(dz, dz, Js);    
-    zhat = zeros(dz, N);
-    
-    Idz = eye(dz);
     
     % Prepend NaN for zero measurement and parameter
     % N.B.: theta is expanded in 'ps' to the same length as y (but
@@ -43,16 +40,10 @@ function [shat, zhat, sys] = smooth_rbffbsi(model, y, theta, Js, sys, par)
     ss = sys(N).x(:, beta);
     shat(:, N) = mean(ss, 2);
     
-    % Initialize sufficient statistics of the backward information filter
-    for j = 1:Js
-        hj = model.py.h(ss(:, j), theta(:, N));
-        Hj = model.py.H(ss(:, j), theta(:, N));
-        Rj = model.py.R(ss(:, j), theta(:, N));
-        lambda_hat(:, j) = Hj'/Rj*(y(:, N) - hj);
-        Omega_hat(:, :, j) = Hj'/Rj*Hj;
-    end
+    % Initialize backward filter
+    [lambda_hat, Omega_hat] = initialize_bf(model, y(:, N), ss, sys(N), theta(:, N));
 
-    % Store particle system
+    % Store
     sys(N).xs = ss;
     sys(N).ws = 1/Js*ones(1, Js);
     sys(N).state = [];
@@ -61,53 +52,65 @@ function [shat, zhat, sys] = smooth_rbffbsi(model, y, theta, Js, sys, par)
     
     %% Backward recursion
     for n = N-1:-1:1
-%         %% Sample
-%         % Backward information filter prediction
-%         [lambda, Omega] = par.predict_bif(model, ss, lambda_hat, Omega_hat);
-%         
+        %% Sample
+        % Backward filter prediction
+        % TODO: For the mixing model, we have to do a backward prediction
+        % for each of the filter particles. This will require some changes
+        % in the interface here and how we handle lambda and Omega.
+        [lambda, Omega] = par.predict_bf(model, ss, lambda_hat, Omega_hat, theta(:, n));
         
-        for j = 1:Js
-            %% Sample backward trajectory
-            % Calculate sufficient statistics
-            % Notation:
-            % Paper -> libsmc
-            % f     -> pz.g
-            % A     -> pz.G
-            % F     -> chol(pz.Q).'
-            gj = model.pz.g(ss(:, j), theta(:, n));
-            Gj = model.pz.G(ss(:, j), theta(:, n));
-            Fj = chol(model.pz.Q(ss(:, j), theta(:, n))).';
-            mj = lambda_hat(:, j) - Omega_hat(:, :, j)*gj;
-            Mj = Fj'*Omega_hat(:, :, j)*Fj + Idz;
-            Lj = Gj'*(Idz - Omega_hat(:, :, j)*Fj/Mj*Fj');
-            lambda = Lj*mj;
-            Omega = Lj*Omega_hat(:, :, j)*Gj;
-            Omega = (Omega + Omega')/2;
-            
-            % Sample
-            [beta, state] = par.sample_backward_simulation(model, ss(:, j), sys(n).x, log(sys(n).w), sys(n).mz, sys(n).Pz, lambda, Omega, theta(n));
-            ss(:, j) = sys(n).x(:, beta);
-            
-            %% Information filter measurement update
-            hj = model.py.h(ss(:, j), theta(:, n));
-            Hj = model.py.H(ss(:, j), theta(:, n));
-            Rj = model.py.R(ss(:, j), theta(:, n));
-            lambda_hat(:, j) = lambda + Hj'/Rj*(y(:, n) - hj);
-            Omega_hat_tmp = Omega + Hj'/Rj*Hj;
-            Omega_hat(:, :, j) = (Omega_hat_tmp + Omega_hat_tmp')/2;
-            
-            %% Store
-            sys(n).lambda(:, j) = lambda;
-            sys(n).Omega(:, :, j) = Omega;
-        end
-        
-        % Point estimate
+        % Sampling
+        [beta, state] = par.sample_backward_simulation(model, ss, sys(n).x, log(sys(n).w), sys(n).mz, sys(n).Pz, lambda, Omega, theta(:, n));
+        ss = sys(n).x(:, beta);
         shat(:, n) = mean(ss, 2);
         
+        % TODO: For mixing models, we have to update lambda and Omega here.
+        
+        % Backward filter update
+        [lambda_hat, Omega_hat] = update_bf(model, y(:, n), ss, lambda, Omega, theta(:, n));
+        
+        %% Old code
+%         for j = 1:Js
+%             %% Sample backward trajectory
+%             % Calculate sufficient statistics
+%             % Notation:
+%             % Paper -> libsmc
+%             % f     -> pz.g
+%             % A     -> pz.G
+%             % F     -> chol(pz.Q).'
+%             gj = model.pz.g(ss(:, j), theta(:, n));
+%             Gj = model.pz.G(ss(:, j), theta(:, n));
+%             Fj = chol(model.pz.Q(ss(:, j), theta(:, n))).';
+%             mj = lambda_hat(:, j) - Omega_hat(:, :, j)*gj;
+%             Mj = Fj'*Omega_hat(:, :, j)*Fj + Idz;
+%             Lj = Gj'*(Idz - Omega_hat(:, :, j)*Fj/Mj*Fj');
+%             lambda = Lj*mj;
+%             Omega = Lj*Omega_hat(:, :, j)*Gj;
+%             Omega = (Omega + Omega')/2;
+%             
+%             % Sample
+%             [beta, state] = par.sample_backward_simulation(model, ss(:, j), sys(n).x, log(sys(n).w), sys(n).mz, sys(n).Pz, lambda, Omega, theta(n));
+%             ss(:, j) = sys(n).x(:, beta);
+%             
+%             %% Information filter measurement update
+%             hj = model.py.h(ss(:, j), theta(:, n));
+%             Hj = model.py.H(ss(:, j), theta(:, n));
+%             Rj = model.py.R(ss(:, j), theta(:, n));
+%             lambda_hat(:, j) = lambda + Hj'/Rj*(y(:, n) - hj);
+%             Omega_hat_tmp = Omega + Hj'/Rj*Hj;
+%             Omega_hat(:, :, j) = (Omega_hat_tmp + Omega_hat_tmp')/2;
+%             
+%             %% Store
+%             sys(n).lambda(:, j) = lambda;
+%             sys(n).Omega(:, :, j) = Omega;
+%         end
+
         %% Store
         sys(n).xs = ss;
         sys(n).ws = 1/Js*ones(1, Js);
         sys(n).state = state;
+        sys(n).lambda = lambda;
+        sys(n).Omega = Omega;
     end
     
     %% Recalculate the linear states
@@ -121,42 +124,110 @@ function [shat, zhat, sys] = smooth_rbffbsi(model, y, theta, Js, sys, par)
     zhat = zhat(:, 2:N);
 end
 
-%% Sampling function
-function [beta, state] = sample_backward_simulation(model, ss, s, lw, mz, Pz, lambda, Omega, theta)
-    [dz, Jf] = size(mz);
-    lZ = zeros(1, Jf);
-    lv = zeros(1, Jf);
-    Iz = eye(dz);
-    state = [];
+%% Backward filter initialization
+function [lambda_hat, Omega_hat] = initialize_bf(model, y, ss, sys, theta)
+    narginchk(5, 5);
+    [dz, Js] = size(sys.mz);
+    lambda_hat = zeros(dz, Js);
+    Omega_hat = zeros(dz, dz, Js);
     
-    %% Calculate ancestor weights
-    % Lambda^i, eta^i according to (21)
-    for i = 1:Jf
-        % TODO: These statistics are calculated in three places; should put them into one function.
-        % Calculate normalizing constant
-        lZ(:, i) = model.ps.logpdf(ss, s(:, i), theta);
-        
-        % Other statistics
-        mzi = mz(:, i);
-%           Pzi = sys(n).Pz(:, :, i);
-%           Gammai = chol(Pzi).';
-        Gammai = chol(Pz(:, :, i));
-        Lambda = Gammai'*Omega*Gammai + Iz;
-        eta = mzi'*Omega*mzi - 2*lambda'*mzi ...
-            - (Gammai'*(lambda - Omega*mzi))'/Lambda*(Gammai'*(lambda - Omega*mzi));
-%           eta = wnorm(mzi, Omega) - 2*lambda'*mzi ...
-%              - wnorm(Gammai'*(lambda - Omega*mzi), Lambda\Iz);
-
-        % Ancestor weight
-        lv(:, i) = -1/2*log(det(Lambda)) - 1/2*eta;
+    for j = 1:Js
+        hj = model.py.h(ss(:, j), theta);
+        Hj = model.py.H(ss(:, j), theta);
+        Rj = model.py.R(ss(:, j), theta);
+        Lj = Hj'/Rj;
+        lambda_hat(:, j) = Lj*(y - hj);
+        Omega_tmp = Lj*Hj;
+        Omega_hat(:, :, j) = (Omega_tmp + Omega_tmp')/2;
     end
-                        
-    %% Sample
-    lws = lw + lZ + lv;
-    ws = exp(lws-max(lws));
-    ws = ws/sum(ws);
-    ir = resample_stratified(ws);
-    beta = ir(randi(Jf, 1));
+end
+
+%% Backward filter prediction
+% Notation:
+% Paper -> libsmc
+% f     -> pz.g
+% A     -> pz.G
+% F     -> chol(pz.Q).'
+function [lambda, Omega] = predict_bf_hierarchical(model, ss, lambda_hat, Omega_hat, theta)
+    narginchk(5, 5);
+    [dz, Js] = size(lambda_hat);
+    lambda = zeros(dz, Js);
+    Omega = zeros(dz, dz, Js);
+    
+    for j = 1:Js
+        gj = model.pz.g(ss(:, j), theta);
+        Gj = model.pz.G(ss(:, j), theta);
+        Fj = chol(model.pz.Q(ss(:, j), theta)).';
+        mj = lambda_hat(:, j) - Omega_hat(:, :, j)*gj;
+        Mj = Fj'*Omega_hat(:, :, j)*Fj + eye(dz);
+        Lj = Gj'*(eye(dz) - Omega_hat(:, :, j)*Fj/Mj*Fj');
+        lambda(:, j) = Lj*mj;
+        Omega_tmp = Lj*Omega_hat(:, :, j)*Gj;
+        Omega(:, :, j) = (Omega_tmp + Omega_tmp')/2;
+    end
+end
+
+%% Backward filter update
+function [lambda_hat, Omega_hat] = update_bf(model, y, ss, lambda, Omega, theta)
+    narginchk(6, 6);
+    [dz, Js] = size(lambda);
+    lambda_hat = zeros(dz, Js);
+    Omega_hat = zeros(dz, Js);
+
+    for j = 1:Js
+        hj = model.py.h(ss(:, j), theta);
+        Hj = model.py.H(ss(:, j), theta);
+        Rj = model.py.R(ss(:, j), theta);
+        Lj = Hj'/Rj;
+        lambda_hat(:, j) = lambda(:, j) + Lj*(y - hj);
+        Omega_tmp = Omega(:, :, j) + Lj*Hj;
+        Omega_hat(:, :, j) = (Omega_tmp + Omega_tmp')/2;
+    end
+end
+
+%% Backward simulation sampling
+% Notation:
+% Paper -> libsmc
+% f     -> pz.g
+% A     -> pz.G
+% F     -> chol(pz.Q).'
+function [beta, state] = sample_backward_simulation(model, ss, s, lw, mz, Pz, lambda, Omega, theta)
+    narginchk(9, 9);
+    state = [];
+    Js = size(ss, 2);
+    [dz, Jf] = size(mz);
+    lv = zeros(1, Jf);
+    beta = zeros(1, Js);
+        
+    for j = 1:Js
+        %% Calculate backward weights
+        % According to (21)
+        for i = 1:Jf
+            % log(Z[n])
+            lZ = model.ps.logpdf(ss(:, j), s(:, i), theta);
+
+            % Lambda[n], (21a)
+            % TODO: Numerically stable implementation
+            mzi = mz(:, i);
+            Gammai = chol(Pz(:, :, i)).';
+            Lambda = Gammai'*Omega(:, :, j)*Gammai + eye(dz);
+            Lambda = (Lambda + Lambda')/2;
+            
+            % eta[n], (21b)
+            % TODO: Numerically stable implementation
+            eta = mzi'*Omega(:, :, j)*mzi - 2*lambda(:, j)'*mzi ...
+                - (Gammai'*(lambda(:, j) - Omega(:, :, j)*mzi))'/Lambda*(Gammai'*(lambda(:, j) - Omega(:, :, j)*mzi));
+    
+            % Weight
+            lv(:, i) = lw(i) + lZ - 1/2*log(det(Lambda)) - 1/2*eta;
+        end
+
+        %% Sample
+        vs = exp(lv-max(lv));
+        vs = vs/sum(vs);
+        ir = resample_stratified(vs);
+        beta(j) = ir(randi(Jf, 1));
+    end
 end
 
 %% Recalculate the linear states
